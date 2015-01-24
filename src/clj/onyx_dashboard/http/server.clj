3886dca264a821fdc61fe5f7721f7631f4337c1c
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-01-24 23:00:14 +0800

    Add watches on ZK root node for deployment listing

--- src/clj/onyx_dashboard/http/server.clj
+++ src/clj/onyx_dashboard/http/server.clj
@@ -15,6 +15,8 @@
             [onyx.system :as system :refer [onyx-client]]
             [onyx.extensions :as extensions]
             [onyx.api]
+            [onyx.log.zookeeper :as zk-onyx]
+            [zookeeper :as zk]
             [taoensso.timbre :as timbre :refer [info error spy]]))
 
 (deftemplate page
@@ -27,7 +29,7 @@
 
 (def replica-state (atom {:replica nil :diff nil}))
 
-(defn track-cluster [sente peer-config uid]
+(defn track-deployment [send-fn! peer-config uid]
   (let [ch (chan 100)
         client (component/start (system/onyx-client peer-config))]
     (loop [replica (extensions/subscribe-to-log (:log client) ch)]
@@ -35,8 +37,7 @@
             entry (extensions/read-log-entry (:log client) position)
             new-replica (extensions/apply-log-entry entry replica)
             diff (extensions/replica-diff entry replica new-replica)]
-        ((:chsk-send! sente) uid [:cluster/replica {:replica new-replica
-                                                    :diff diff}])
+        (send-fn! uid [:deployment/replica {:replica new-replica :diff diff}])
         (recur new-replica)))))
 
 (def env-conf
@@ -47,40 +48,61 @@
    :onyx.peer/job-scheduler :onyx.job-scheduler/round-robin 
    :onyx/id :SUPPLY_ME})
 
-(comment (reset! replica-state nil)
-         (deref replica-state)
-         (clojure.pprint/pprint {:orig orig :new new}))
+(defn zk-deployment-entry-stat [client entry]
+  (:stat (zk/data client (zk-onyx/prefix-path entry))))
+
+(def deployments (atom {}))
+(def tracking (atom {}))
+
+(defn distribute-deployments [send-all-fn! deployments]
+  (send-all-fn! [:deployment/listing deployments]))
+
+(defn refresh-deployments-watch [send-all-fn! zk-client]
+  (->> (zk/children zk-client zk-onyx/root-path :watcher (fn [_] (refresh-deployments-watch send-all-fn! zk-client)))
+       (map (juxt identity 
+                  (partial zk-deployment-entry-stat zk-client)))
+       (map (fn [[child stat]]
+              (vector child
+                      {:created (java.util.Date. (:ctime stat))
+                       :modified (java.util.Date. (:mtime stat))})))
+       (into {})
+       (reset! deployments)
+       (distribute-deployments send-all-fn!)))
 
 (defn event->uid [event]
   (get-in event [:ring-req :cookies "ring-session" :value]))
 
-(def tracking (atom {}))
-
 (defn launch-event-handler [sente peer-config]
   (thread
     (loop []
-      (let [[event _] (alts!! [(:ch-chsk sente) 
-                               #_(:ring-ajax-get-or-ws-handshake sente)])]
-        (when event
-          (info "EVENT:" event)
-          ;; TODO: more sophisticated tracking,
-          ;; should track by cluster id rather than user
-          ;; and count the number of users tracking it. When the user count drops to 0
-          ;; then stop the future.
+      (when-let [event (<!! (:ch-chsk sente))]
+        (info "EVENT:" event)
+        ;; TODO: more sophisticated tracking,
+        ;; should track by cluster id rather than user
+        ;; and count the number of users tracking it. When the user count drops to 0
+        ;; then stop the future.
+        (let [user-id (event->uid event)] 
           (case (:id event) 
-            :cluster/track (let [user-id (event->uid event)] 
-                             (swap! tracking 
-                                    assoc 
-                                    user-id
-                                    (future (track-cluster sente 
-                                                           (assoc env-conf :onyx/id (:?data event)) 
-                                                           user-id))))
-            :chsk/uidport-close (let [user-id (event->uid event)] 
+            :deployment/track (do (when-let [fut (@tracking user-id)]
+                                    (future-cancel fut))
+                                  (swap! tracking 
+                                         assoc 
+                                         user-id
+                                         (future (track-deployment (:chsk-send! sente)
+                                                                   (assoc env-conf :onyx/id (:?data event)) 
+                                                                   user-id))))
+            :deployment/get-listing ((:chsk-send! sente) user-id [:deployment/listing @deployments])
+            :chsk/uidport-close (do
                                   (future-cancel (@tracking user-id))
                                   (swap! tracking dissoc user-id)) 
-            (println "Dunno what to do with: " event))
-          (recur))))))
+            (println "Dunno what to do with: " event)))
+        (recur)))))
 
+(defn send-mult-fn [send-fn! connected-uids msg]
+  (doseq [uid (:any @connected-uids)]
+    (send-fn! uid msg)))
+
+; no need for env-config and peer-config
 (defrecord HttpServer [peer-config]
   component/Lifecycle
   (start [{:keys [sente] :as component}]
@@ -90,13 +112,16 @@
       (GET  "/" [] (page))
       (GET  "/chsk" req ((:ring-ajax-get-or-ws-handshake sente) req))
       (POST "/chsk" req ((:ring-ajax-post sente) req))
-      ;(GET "/cluster/:cluster-id" req (get-job-output req))
+      ;(GET "/cluster/:deployment-id" req (get-job-output req))
       ;(GET "/job/:job-id" req (get-job-output req))
       (resources "/")
       (resources "/react" {:root "react"})
       (route/not-found "Page not found"))
-
     (launch-event-handler sente peer-config)
+    (refresh-deployments-watch (partial send-mult-fn 
+                                        (:chsk-send! sente) 
+                                        (:connected-uids sente)) 
+                               (zk/connect (:zookeeper/address env-conf)))
 
     (let [my-ring-handler (ring.middleware.defaults/wrap-defaults my-routes ring-defaults-config)
           server (http-kit-server/run-server my-ring-handler {:port 3000})
