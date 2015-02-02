adf12e77bb4378d6c958a69d932364a979e6077c
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-02-02 10:01:33 +0800

    Non snapshot onyx release.

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
                  ;[org.danielsz/system "0.1.4"]
                  [cljs-uuid "0.0.4"]
                  [ring "1.3.2"]
-                 [com.mdrogalis/onyx "0.5.1-SNAPSHOT"]
+                 [com.mdrogalis/onyx "0.5.1"]
                  [ring/ring-defaults "0.1.3"]
                  [compojure "1.3.1"]
                  [enlive "1.1.5"]
