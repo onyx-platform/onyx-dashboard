86309fd4c75f56b27d70fd57697958a049bd9544
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-02-13 01:29:16 +0800

    Formatting

--- src/cljs/onyx_dashboard/controllers/websocket.cljs
+++ src/cljs/onyx_dashboard/controllers/websocket.cljs
@@ -3,31 +3,36 @@
 
 (defmulti msg-controller (fn [[type _] _] type))
 
-(defmethod msg-controller :deployment/listing [[_ msg] state]
+(defmethod msg-controller :deployment/listing 
+  [[_ msg] state]
   (assoc-in state [:deployments] msg))
 
 (defn is-tracking? [msg state]
   (= (:tracking-id msg)
      (get-in state [:deployment :tracking-id])))
 
-(defmethod msg-controller :deployment/up-to-date [[_ {:keys [tracking-id last-id] :as msg}] state]
+(defmethod msg-controller :deployment/up-to-date 
+  [[_ {:keys [tracking-id last-id] :as msg}] state]
   (if (and (is-tracking? msg state)
            (= last-id (:message-id-max (:deployment state))))
     (assoc-in state [:deployment :up-to-date?] true)
     state))
 
-(defmethod msg-controller :deployment/submitted-job [[_ {:keys [id] :as msg}] state]
+(defmethod msg-controller :deployment/submitted-job 
+  [[_ {:keys [id] :as msg}] state]
   (if (is-tracking? msg state)
     (assoc-in state [:deployment :jobs id] (assoc msg :status :submitted)) 
     state))
 
-(defmethod msg-controller :job/peer-assigned [[_ {:keys [peer-id task job-id] :as msg}] state]
+(defmethod msg-controller :job/peer-assigned 
+  [[_ {:keys [peer-id task job-id] :as msg}] state]
   ;(println "Peer assigned " msg)
   (if (is-tracking? msg state)
     (assoc-in state [:deployment :jobs job-id :tasks peer-id] task)
     state))
 
-(defmethod msg-controller :deployment/peer-joined [[_ {:keys [id] :as msg}] state]
+(defmethod msg-controller :deployment/peer-joined 
+  [[_ {:keys [id] :as msg}] state]
   ;(println "Peer joined " msg)
   (if (is-tracking? msg state)
     (update-in state [:deployment :peers] union #{id}) 
@@ -39,7 +44,8 @@
 ;     (update-in state [:deployment :peers] union #{id}) 
 ;     state))
 
-(defmethod msg-controller :deployment/peer-notify-joined-accepted [[_ {:keys [id] :as msg}] state]
+(defmethod msg-controller :deployment/peer-notify-joined-accepted 
+  [[_ {:keys [id] :as msg}] state]
   ;(println "Peer immediate notify joined " msg)
   (if (is-tracking? msg state)
     (update-in state [:deployment :peers] union #{id}) 
@@ -52,13 +58,15 @@
                (vals jobs))))
 
 
-(defmethod msg-controller :deployment/log-replay-crash [[_ {:keys [id] :as msg}] state]
+(defmethod msg-controller :deployment/log-replay-crash 
+  [[_ {:keys [id] :as msg}] state]
   ;(println "Log replay crashed" msg)
   (if (is-tracking? msg state)
     (assoc-in state [:deployment :status] {:status :crashed :error (:error msg)})
     state))
 
-(defmethod msg-controller :deployment/peer-left [[_ {:keys [id] :as msg}] state]
+(defmethod msg-controller :deployment/peer-left 
+  [[_ {:keys [id] :as msg}] state]
   ;(println "Peer left" msg)
   (if (is-tracking? msg state)
     (-> state 
@@ -66,13 +74,15 @@
         (update-in [:deployment :jobs] remove-dead-peer id)) 
     state))
 
-(defmethod msg-controller :job/completed-task [[_ {:keys [peer-id job-id task] :as msg}] state]
+(defmethod msg-controller :job/completed-task 
+  [[_ {:keys [peer-id job-id task] :as msg}] state]
   ;(println "Completed task " msg)
   (if (is-tracking? msg state)
     (update-in state [:deployment :jobs job-id :tasks] dissoc peer-id) 
     state))
 
-(defmethod msg-controller :deployment/kill-job [[_ {:keys [id] :as msg}] state]
+(defmethod msg-controller :deployment/kill-job 
+  [[_ {:keys [id] :as msg}] state]
   ;; Managed to create a kill-job entry with a nil job. Should fix this upstream
   ;; {:entry {:args {:job nil}, :fn :kill-job, :message-id 63, :created-at 1423218732944}}}
   (if (and (is-tracking? msg state) id)
@@ -85,7 +95,8 @@
           state
           job-ids))
 
-(defmethod msg-controller :deployment/job-statuses [[_ {:keys [finished-jobs incomplete-jobs] :as msg}] state]
+(defmethod msg-controller :deployment/job-statuses 
+  [[_ {:keys [finished-jobs incomplete-jobs] :as msg}] state]
   (if (is-tracking? msg state)
     (-> state
         (update-statuses finished-jobs :finished)
