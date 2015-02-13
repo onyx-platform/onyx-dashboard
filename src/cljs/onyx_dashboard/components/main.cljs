32959b710e4a6ea583038991811cb5d408a60283
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-02-14 00:32:58 +0800

    Move the header out of the main grid

--- src/cljs/onyx_dashboard/components/main.cljs
+++ src/cljs/onyx_dashboard/components/main.cljs
@@ -23,52 +23,52 @@
   (render-state [_ {:keys [api-chan]}]
                 (let [{:keys [selected-job jobs]} deployment
                       job (and selected-job jobs (jobs selected-job))] 
-                  (g/grid {}
-                          (g/row {}
-                                 (r/page-header {:class "page-header, main-header"}
-                                                (g/grid {}
-                                                        (g/col {:xs 4}
-                                                               (dom/a {:href "https://github.com/MichaelDrogalis/onyx"}
-                                                                      (dom/img {:class "logo" 
-                                                                                :src "/img/high-res.png" 
-                                                                                :height 60})))
-                                                        (g/col {:xs 8}
-                                                               (dom/div {:style {:font-size "42px" 
-                                                                                 :margin-top "10px"}}
-                                                                        "Onyx Dashboard")))))
-                          (g/row {}
-                                 (g/col {:xs 6 :md 4}
-                                        (dom/div {:class "left-nav-deployment"} 
-                                                 (om/build select-deployment app {}))
+                  (dom/div
+                    (r/page-header {:class "page-header, main-header"}
+                                   (g/grid {}
+                                           (g/col {:xs 6 :md 4}
+                                                  (dom/a {:href "https://github.com/MichaelDrogalis/onyx"}
+                                                         (dom/img {:class "logo" 
+                                                                   :src "/img/high-res.png" 
+                                                                   :height 60})))
+                                           (g/col {:xs 12 :md 8}
+                                                  (dom/div {:style {:font-size "42px" 
+                                                                    :margin-top "10px"}}
+                                                           "Onyx Dashboard"))))
+                    (g/grid {}
+                            (g/row {}
+                                   (g/col {:xs 6 :md 4}
+                                          (dom/div {:class "left-nav-deployment"} 
+                                                   (om/build select-deployment app {}))
 
-                                        (if (:id deployment) 
-                                          (dom/div {} 
-                                                   (om/build deployment-indicator 
-                                                             {:deployment deployment
-                                                              :last-entry ((:entries deployment) (:message-id-max deployment))} 
-                                                             {})))
+                                          (if (:id deployment) 
+                                            (dom/div {} 
+                                                     (om/build deployment-indicator 
+                                                               {:deployment deployment
+                                                                :last-entry ((:entries deployment) (:message-id-max deployment))} 
+                                                               {})))
 
-                                        (if (:id deployment)
-                                          (dom/div {} 
-                                                   (om/build job-selector deployment {})))
+                                          (if (:id deployment)
+                                            (dom/div {} 
+                                                     (om/build job-selector deployment {})))
 
-                                        (if (:id deployment) 
-                                          (dom/div {} 
-                                                   (om/build deployment-peers deployment {})))
+                                          (if (:id deployment) 
+                                            (dom/div {} 
+                                                     (om/build deployment-peers deployment {})))
 
-                                        (dom/div {}
-                                                 (if job 
-                                                   (om/build job-management 
-                                                             job
-                                                             {:react-key (str "management-" (:id job))}))))
-                                 (g/col {:xs 12 :md 8}
-                                        (dom/div 
-                                          (if job 
-                                            (om/build job-info   
-                                                      (if (:up? deployment) job (dissoc job :tasks :peers))
-                                                      {:react-key (str "job-info-" (:id job))}))
+                                          (dom/div {}
+                                                   (if job 
+                                                     (om/build job-management 
+                                                               job
+                                                               {:react-key (str "management-" (:id job))}))))
+                                   (g/col {:xs 11 :md 8}
+                                          (dom/div 
+                                            (if job 
+                                              (om/build job-info   
+                                                        (if (:up? deployment) job (dissoc job :tasks :peers))
+                                                        {:react-key (str "job-info-" (:id job))}))
 
-                                          (om/build log-entries-pager 
-                                                    {:entries (:entries deployment)
-                                                     :job-filter (:id job)} 
-                                                    {:react-key (str "log-" (:id deployment) "-filter-" (:id job))}))))))))
+                                            (om/build log-entries-pager 
+                                                      {:entries (:entries deployment)
+                                                       :job-filter (:id job)} 
+                                                      {:react-key (str "log-" (:id deployment) "-filter-" (:id job))})))))))))
