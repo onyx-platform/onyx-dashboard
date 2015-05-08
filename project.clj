2163cb972f0c308c8b8f0ac96658ea26a0162c33
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-05-08 13:07:38 +0800

    Fix uberjar

--- project.clj
+++ project.clj
@@ -17,10 +17,13 @@
                  [prismatic/schema "0.3.7"]
                  [com.stuartsierra/component "0.2.2"]
                  [com.taoensso/sente "1.4.1"]
+                 ; Pin encore to the same version used by nippy/onyx
+                 ; not sente
+                 [com.taoensso/encore "1.28.0"]
                  [com.taoensso/timbre "3.3.1"]
                  [cljs-uuid "0.0.4"]
                  [ring "1.3.2"]
-                 [com.mdrogalis/onyx "0.6.0-alpha2"]
+                 [com.mdrogalis/onyx "0.6.0-alpha2" :exclusions [Agrona aeron-client aeron-driver aeron-common]]
                  [com.cognitect/transit-clj "0.8.259"]
                  [com.cognitect/transit-cljs "0.8.205"]
                  [cljsjs/moment "2.9.0-0"]
