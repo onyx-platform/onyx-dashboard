ec803bb94c7225723fee36f84a226ac20404a097
Michael Drogalis <mjd3089@rit.edu>
2015-09-20 10:58:15 -0700

    Prepare for release.

---
+++ scripts/release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+cd "$(dirname "$0")/.."
+
+new_version=$1
+release_branch=$2
+current_version=`lein pprint :version`
+
+# Update to release version.
+git checkout master
+lein set-version $new_version
+sed -i '' "s/$current_version/$new_version/g" README.md
+
+git commit -am "Release version $new_version."
+git tag $new_version
+git push origin $new_version
+git push origin master
+
+# Merge artifacts into release branch.
+git checkout $release_branch
+git merge master -X theirs
+git push origin $release_branch
+
+# Prepare next release cycle.
+git checkout master
+lein set-version
+git commit -m "Prepare for next release cycle." project.clj README.md
+git push origin master
