9dc696970951db096d25d2951181c0b0729651dc
Lucas Bradstreet <lucas.bradstreet@onyxplatform.org>
2015-09-22 01:21:25 +0300

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-dashboard "0.7.5.2"
+(defproject org.onyxplatform/onyx-dashboard "0.7.5.3-SNAPSHOT"
   :description "Dashboard for the Onyx distributed computation system"
   :url "http://github.com/lbradstreet/onyx-dashboard"
   :license {:name "Eclipse Public License"
