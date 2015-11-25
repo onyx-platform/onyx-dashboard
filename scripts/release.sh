1a0f4b8ed0c5d271d4a31aa439952d1fbc70fa6c
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-25 16:24:32 +0800

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,24 +7,58 @@ set -o xtrace
 
 cd "$(dirname "$0")/.."
 
+if [[ "$#" -ne 2 ]]; then
+	echo "Usage: $0 new-version release-branch"
+	echo "Example: $0 0.8.0.4 0.8.x"
+	echo "Three digit release number e.g. 0.8.0 will update onyx dependency and release plugin as 0.8.0.0"
+fi
+
 new_version=$1
+
+version_type=$(echo "$1"|sed s/".*-"//g)
+version_base=$(echo "$1"|sed s/"-.*"//g)
+
 release_branch=$2
-current_version=`lein pprint :version`
+current_version=`lein pprint :version | sed s/\"//g`
 
 # Update to release version.
 git checkout master
-lein set-version $new_version
-sed -i '' "s/$current_version/$new_version/g" README.md
+git stash
+git pull
 
-git commit -am "Release version $new_version."
-git tag $new_version
-git push origin $new_version
+if [[ "$new_version" == *[.]*[.]*[.]* ]]; 
+then 
+	echo "Four digit release number "$new_version" therefore releasing plugin without updating Onyx dependency"
+	new_plugin_version=$new_version
+elif [[ "$new_version" == *[.]*[.]* ]]; 
+then
+	core_version=$new_version
+	lein update-dependency org.onyxplatform/onyx $core_version
+	if [[ "$version_type" == "" ]]; then 
+		new_plugin_version=$version_base".0"
+	else
+		new_plugin_version=$version_base".0-"$version_type
+	fi
+
+else
+	echo "Unhandled version number scheme. Exiting"
+	exit 1
+fi
+
+lein set-version $new_plugin_version
+
+sed -i.bak "s/$current_version/$new_plugin_version/g" README.md
+git add README.md project.clj
+
+git commit -m "Release version $new_plugin_version."
+git tag $new_plugin_version
+git push origin $new_plugin_version
 git push origin master
 
 # Merge artifacts into release branch.
-git checkout $release_branch
-git merge master -X theirs
-git push origin $release_branch
+git checkout -b $release_branch
+git merge -m "Merge branch 'master' into $release_branch" master -X theirs
+git push -u origin $release_branch
 
 # Prepare next release cycle.
 git checkout master
