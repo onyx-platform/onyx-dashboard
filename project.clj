b9bbc89f9a316b05deb600b539317f992f97d649
OnyxBot <michael.drogalis@onyxplatform.org>
2015-11-26 20:42:39 +0000

    Update dependency org.onyxplatform/onyx to version 0.8.3-20151126_201941-gac7a377.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                  [cljs-uuid "0.0.4"]
                  [ring "1.3.2"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.8.3-20151126_192806-g6592bd5"]
+                 [org.onyxplatform/onyx "0.8.3-20151126_201941-gac7a377"]
                  [com.cognitect/transit-clj "0.8.275"]
                  [com.cognitect/transit-cljs "0.8.220"]
                  [cljsjs/moment "2.9.0-0"]
