19922939c413f0e41810d37b1afc1040320d196a
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-16 02:47:18 +0800

    Versioned lib-onyx

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
 		 [ring "1.3.2"]
 		 ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
 		 [org.onyxplatform/onyx "0.8.11"]
-		 [org.onyxplatform/lib-onyx "0.8.12.0-SNAPSHOT" :exclusions [ring-jetty-component]]
+		 [org.onyxplatform/lib-onyx "0.8.11.0" :exclusions [ring-jetty-component]]
                  [org.onyxplatform/onyx-visualization "0.1.0"]
 		 [timothypratley/patchin "0.3.5"]
 		 [com.cognitect/transit-clj "0.8.275"]
