e0a798a903d0c827dac3e950c05484e07b6f4dad
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-16 19:03:35 +0800

    Fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -62,7 +62,7 @@ cat RELEASES.MD >> NEWRELEASES.MD
 mv -f NEWRELEASES.MD RELEASES.MD
 
 sed -i.bak "s/$current_version/$new_plugin_version/g" README.md
-git add README.md project.clj
+git add README.md project.clj RELEASES.MD
 
 git commit -m "Release version $new_plugin_version."
 git tag $new_plugin_version
