6fd0dfef8aa51a84b52f118fc3ba9d43f58b43d2
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-07-08 17:01:48 +0800

    Fix dashboard operation for 0.9.7. Broken by task-ids

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
 		 [ring "1.3.2"]
 		 ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
 		 [org.onyxplatform/onyx "0.9.7"]
-		 [org.onyxplatform/lib-onyx "0.8.11.0" :exclusions [ring-jetty-component org.onyxplatform/onyx]]
+		 [org.onyxplatform/lib-onyx "0.9.7.1" :exclusions [ring-jetty-component org.onyxplatform/onyx]]
                  [org.onyxplatform/onyx-visualization "0.1.0"]
 		 [timothypratley/patchin "0.3.5"]
 		 [com.cognitect/transit-clj "0.8.285"]
