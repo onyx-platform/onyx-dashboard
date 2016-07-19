2c21cba804afc16704b32c3007b8b7e4d5397447
OnyxBot <michael.drogalis@onyxplatform.org>
2016-07-19 01:59:01 +0000

    Update dependency org.onyxplatform/onyx to version 0.9.10-20160719_013519-g5201b16.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
 		 [ring "1.3.2"]
 		 ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-		 [org.onyxplatform/onyx "0.9.9"]
+		 [org.onyxplatform/onyx "0.9.10-20160719_013519-g5201b16"]
 		 [org.onyxplatform/lib-onyx "0.9.7.1" :exclusions [ring-jetty-component org.onyxplatform/onyx]]
                  [org.onyxplatform/onyx-visualization "0.1.0"]
 		 [timothypratley/patchin "0.3.5"]
