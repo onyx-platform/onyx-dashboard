2fe30bc5bc67915433287958c8c73c399da5e0ad
OnyxBot <michael.drogalis@onyxplatform.org>
2017-03-19 08:20:29 +0000

    Update dependency org.onyxplatform/onyx to version 0.10.0-beta8.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -26,7 +26,7 @@
 
                  ; onyx deps
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.10.0-20170319_075336-gd6f4773"]
+                 [org.onyxplatform/onyx "0.10.0-beta8"]
                  [org.onyxplatform/lib-onyx "0.9.10.0" :exclusions [ring-jetty-component org.onyxplatform/onyx]]
                  [org.onyxplatform/onyx-visualization "0.4.0"]
 
