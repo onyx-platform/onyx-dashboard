4333fdbd222e725dca71f430d9ee6925f1335a41
OnyxBot <michael.drogalis@onyxplatform.org>
2017-05-18 01:10:23 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-dashboard "0.10.0.0-beta16"
+(defproject org.onyxplatform/onyx-dashboard "0.10.0.0-SNAPSHOT"
   :description "Dashboard for the Onyx distributed computation system"
   :url "http://github.com/lbradstreet/onyx-dashboard"
   :license {:name "Eclipse Public License"
