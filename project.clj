b965fdba3855e8b4586235d98db0e376c89d2033
OnyxBot <michael.drogalis@onyxplatform.org>
2017-10-16 19:11:43 +0000

    Update dependency org.onyxplatform/onyx to version 0.11.1-alpha2.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -26,7 +26,7 @@
 
                  ; onyx deps
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.11.1-20171016_181013-gab48b62"]
+                 [org.onyxplatform/onyx "0.11.1-alpha2"]
                  [org.onyxplatform/lib-onyx "0.9.10.0" :exclusions [ring-jetty-component org.onyxplatform/onyx]]
                  [org.onyxplatform/onyx-visualization "0.4.0"]
 
