44a316d6328ead3874211b3fb75b7d8586e4cca0
OnyxBot <michael.drogalis@onyxplatform.org>
2018-09-27 05:27:43 +0000

    Update dependency org.onyxplatform/onyx to version 0.13.6-20180927_051814-g7f540fe.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -24,7 +24,7 @@
 
                  ; onyx deps
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.13.5"]
+                 [org.onyxplatform/onyx "0.13.6-20180927_051814-g7f540fe"]
                  [org.onyxplatform/lib-onyx "0.9.10.0" :exclusions [ring-jetty-component org.onyxplatform/onyx]]
                  [org.onyxplatform/onyx-visualization "0.5.0"]
 
